49334cceeb67884ef45a7f4979db166778892d9d
Miguel Angel Femenia <mfemvaz2906@g.educaand.es>
2023-03-08 11:06:42 +0100

    update

--- src/main/java/com/mafv/portal/configuration/SecurityConfig.java
+++ src/main/java/com/mafv/portal/configuration/SecurityConfig.java
@@ -4,47 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    // Noop -> Que no tenga codificación
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        //return NoOpPasswordEncoder.getInstance();
     }
 
     /*@Bean
     UsuariosService myUserService(){
         return new UsuariosService();
     }*/
-
-    @Bean
-    public UserDetailsService user(){
-
-        UserDetails user = User.builder()
-            .username("user")
-            .password("{noop}user*1234")
-            .authorities("USER")
-            .build();
-
-        UserDetails admin = User.builder()
-            .username("admin")
-            .password("{noop}admin*1234")
-            .authorities("USER","ADMIN")
-            .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider(){
